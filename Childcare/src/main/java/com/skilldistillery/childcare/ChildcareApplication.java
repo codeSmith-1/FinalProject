de87d402e8d71d18ab4073b77677fbf14a5a97f8
Tate Smith <thsmith.cofc@gmail.com>
2022-11-22 12:50:56 -0500

    Create Boot/REST project, connect dependency

---
+++ Childcare/src/main/java/com/skilldistillery/childcare/ChildcareApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.childcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildcareApplication.class, args);
+	}
+
+}
