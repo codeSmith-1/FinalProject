7452f6e305d3ce1f0dc1d7e1fa397f74fff8ec22
Andrew Shaw <shandyshawshank@gmail.com>
2022-11-23 10:56:11 -0700

    created some repositories. Updated persistence xml to include all entities

---
+++ Childcare/src/main/java/com/skilldistillery/childcare/repositories/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.skilldistillery.childcare.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.skilldistillery.childcare.entities.Address;
+
+public interface AddressRepository extends JpaRepository<Address, Integer> {
+
+}
