fc77e1c6a867ebc6916af5c609352885cfe02f4f
Tate Smith <thsmith.cofc@gmail.com>
2022-11-23 12:08:12 -0500

    bunch o tests

---
+++ JPAChildcare/src/test/java/com/skilldistillery/childcare/entities/MoodTest.java
@@ -0,0 +1,48 @@
+package com.skilldistillery.childcare.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MoodTest {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Mood mood;
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAChildcare");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	em = emf.createEntityManager();
+	mood = em.find(Mood.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		mood = null;
+	}
+
+	@Test
+	void test() {
+		assertNotNull(mood);
+		assertEquals("asdf", mood.getDescription());
+	}
+
+}
