82632f83f14256103721db71b8b063f8c7cc6ef9
AshNlandofOzz <ashleyosburn88@gmail.com>
2022-11-30 12:03:29 -0700

    updates

--- ngDaycare/src/app/models/kid.ts
+++ ngDaycare/src/app/models/kid.ts
@@ -1,16 +1,12 @@
-import { Classroom } from "./classroom";
-
 export class Kid {
   id: number;
   firstName: string | undefined;
   lastName: string | undefined;
   birthday: Date | undefined;
   imageUrl: string | undefined;
-  classroom: Classroom;
 
-  constructor(id: number = 0, classroom: Classroom = new Classroom(), firstName?: string, lastName?: string, birthday?: Date, imageUrl?: string){
+  constructor(id: number = 0, firstName?: string, lastName?: string, birthday?: Date, imageUrl?: string){
     this.id = id;
-    this.classroom = classroom;
     this.firstName = firstName;
     this.lastName = lastName;
     this.birthday = birthday;
