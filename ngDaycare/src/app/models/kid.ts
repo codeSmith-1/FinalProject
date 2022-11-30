c5ba91c4d18c937de269fa3389398205f09b4b97
Tate Smith <thsmith.cofc@gmail.com>
2022-11-30 15:23:33 -0500

    kid.ts

--- ngDaycare/src/app/models/kid.ts
+++ ngDaycare/src/app/models/kid.ts
@@ -1,16 +1,19 @@
+import { Classroom } from "./classroom";
+
 export class Kid {
   id: number;
   firstName: string | undefined;
   lastName: string | undefined;
   birthday: Date | undefined;
   imageUrl: string | undefined;
-  classroom: Classroom;
+  classroom: Classroom | null;
 
-  constructor(id: number = 0, classroom: Classroom = new Classroom(), firstName?: string, lastName?: string, birthday?: Date, imageUrl?: string){
+  constructor(id: number = 0, classroom: Classroom | null = null, firstName?: string, lastName?: string, birthday?: Date, imageUrl?: string){
     this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
     this.birthday = birthday;
     this.imageUrl = imageUrl;
+    this.classroom = classroom;
   }
 }
